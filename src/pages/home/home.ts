a28035bcfee4a4bd288f211b793bf56f56577758
tee1mtp <emilie.teitz@us.bosch.com>
2018-09-10 09:53:28 -0400

    API commented out of this version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {GalleryPage} from '../gallery/gallery';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -8,12 +10,39 @@ import {GalleryPage} from '../gallery/gallery';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+posts: any;
 
+  constructor(public navCtrl: NavController,
+              public http: Http) {
+                this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=20').map(res => res.json()).subscribe(data => {
+                        this.posts = data.data.children;
+                        console.log(this.posts)
+              });
   }
 
   showImages(){
     this.navCtrl.push( GalleryPage );
   }
 
+
+//   this.http.get('http://ionic.io', {}, {})
+//     .then(data => {
+//
+//       console.log(data.status);
+//       console.log(data.data); // data received by server
+//       console.log(data.headers);
+//       console.log("Successful");
+//     })
+//     .catch(error => {
+//
+//       console.log(error.status);
+//       console.log(error.error); // error message as string
+//       console.log(error.headers);
+//       console.log("Failure")
+//     });
+// public loadJCrew(){
+//   console.log('Load JCrew');
+// }
+//
+// this.loadJCrew();
 }
