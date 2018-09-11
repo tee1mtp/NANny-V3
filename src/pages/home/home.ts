0dbe0109ffba7b85421d74f942343ccadfe4f9b8
tee1mtp <emilie.teitz@us.bosch.com>
2018-09-11 10:32:06 -0400

    Attempt at Google Maps that is not working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {GalleryPage} from '../gallery/gallery';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
+// import {Http} from '@angular/http';
+// import 'rxjs/add/operator/map';
+
+
 
 @Component({
   selector: 'page-home',
@@ -10,21 +12,45 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-posts: any;
+// Below Code is required for API example
+// posts: any;
 
-  constructor(public navCtrl: NavController,
-              public http: Http) {
-                this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=20').map(res => res.json()).subscribe(data => {
-                        this.posts = data.data.children;
-                        console.log(this.posts)
-              });
+@ViewChild('map') mapElement: ElementRef;
+ map: any;
+ google: any;
+
+  constructor(public navCtrl: NavController) {
+    // Below Code is required for API example
+  //             public http: Http) {
+  //               this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=20').map(res => res.json()).subscribe(data => {
+  //                       this.posts = data.data.children;
+  //                       console.log(this.posts)
+  //             });
   }
 
   showImages(){
     this.navCtrl.push( GalleryPage );
   }
 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
 
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+// Below Code is required for API example
 //   this.http.get('http://ionic.io', {}, {})
 //     .then(data => {
 //
